23b8a8a044419894362a642867b865034206704c
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-26 13:30:35 +0900

    Show empty terminal until key press

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,7 +1,12 @@
 (ns editor.core
   (:gen-class))
 
+(require '[lanterna.terminal :as t])
+
+(def term (t/get-terminal :text))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (t/start term)
+  (t/get-key-blocking term))
