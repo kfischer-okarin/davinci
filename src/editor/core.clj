7610747a79718f8b58e73f705fb58cb8fc65cd61
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-26 23:16:06 +0900

    Open file and display in terminal until keypress

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,12 +1,27 @@
 (ns editor.core
   (:gen-class))
 
+(def buffer (atom []))
+
+(defn open-file [filename]
+  (reset! buffer (clojure.string/split (slurp filename) #"\n")))
+
 (require '[lanterna.terminal :as t])
 
 (def term (t/get-terminal :text))
 
+(defn render-buffer
+  []
+  (let [[w h] (t/get-size term)]
+    (t/move-cursor term 0 0)
+    (doseq [line (take h @buffer)]
+      (t/put-string term (str line \newline)))))
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (t/start term)
-  (t/get-key-blocking term))
+  ([] (println "No args"))
+  ([filename]
+   (open-file filename)
+   (t/in-terminal term
+                  (render-buffer)
+                  (t/get-key-blocking term))))
