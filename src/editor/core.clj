c6b28a71f5db58a35f521705d9ab913974ff2088
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-27 22:26:55 +0900

    Rewrite to use one single editor value that is transformed

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,51 +1,55 @@
 (ns editor.core
   (:gen-class))
 
-(def buffer (atom []))
+(def editor (atom {:buffer []
+                   :cursor [0 0]
+                   :key-bindings {}
+                   :running true}))
 
-(def key-bindings (atom {}))
+(defn quit-editor [editor]
+  (assoc editor :running false))
 
-(def cursor (atom [0 0]))
-(defn move-cursor [dx dy]
-  (swap! cursor #(let [[x y] %] [(+ x dx) (+ y dy)])))
+(defn move-cursor [dx dy editor]
+  (update editor :cursor
+          (fn [[x y]] [(+ x dx) (+ y dy)])))
 
-(def will-quit (atom false))
-(defn quit-editor [] (reset! will-quit true))
+(defn open-file [filename editor]
+  (assoc editor :buffer
+         (clojure.string/split (slurp filename) #"\n")))
 
-(defn open-file [filename]
-  (reset! buffer (clojure.string/split (slurp filename) #"\n")))
+(defn bind-key [key action]
+  (swap! editor #(assoc-in % [:key-bindings key] action)))
+
+(defn execute-action [key editor]
+  (let [action (get-in editor [:key-bindings key])]
+    (if action
+      (action editor)
+      editor)))
+
+(bind-key \q quit-editor)
+(bind-key :up (partial move-cursor 0 -1))
+(bind-key :right (partial move-cursor 1 0))
+(bind-key :left (partial move-cursor -1 0))
+(bind-key :down (partial move-cursor 0 1))
 
 (require '[lanterna.terminal :as t])
 
-(def term (t/get-terminal :text))
-
-(defn render-buffer
-  []
+(defn render-in-terminal
+  [editor term]
   (let [[w h] (t/get-size term)]
     (t/move-cursor term 0 0)
-    (doseq [line (take h @buffer)]
+    (doseq [line (take h (:buffer editor))]
       (t/put-string term (str line \newline)))
-    (let [[x y] @cursor]
+    (let [[x y] (:cursor editor)]
       (t/move-cursor term x y))))
-
-(defn bind-key [key action]
-  (swap! key-bindings #(assoc % key action)))
-
-(defn execute-action [key]
-  (let [action (get @key-bindings key)] (if action (action))))
-
-(bind-key \q (fn [] (quit-editor)))
-(bind-key :up (fn [] (move-cursor 0 -1)))
-(bind-key :right (fn [] (move-cursor 1 0)))
-(bind-key :left (fn [] (move-cursor -1 0)))
-(bind-key :down (fn [] (move-cursor 0 1)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   ([] (println "No args"))
   ([filename]
-   (open-file filename)
-   (t/in-terminal term
-                  (while (not @will-quit)
-                    (render-buffer)
-                    (execute-action (t/get-key-blocking term))))))
+   (swap! editor (partial open-file filename))
+   (let [term (t/get-terminal :text)]
+     (t/in-terminal term
+                    (while (:running @editor)
+                      (render-in-terminal @editor term)
+                      (swap! editor (partial execute-action (t/get-key-blocking term))))))))
