924fd1f166c5d4de383c0872b0ad13113181ff24
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2020-11-27 17:02:09 +0900

    Add basic key-binding loop + cursor movement + quit

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -3,6 +3,15 @@
 
 (def buffer (atom []))
 
+(def key-bindings (atom {}))
+
+(def cursor (atom [0 0]))
+(defn move-cursor [dx dy]
+  (swap! cursor #(let [[x y] %] [(+ x dx) (+ y dy)])))
+
+(def will-quit (atom false))
+(defn quit-editor [] (reset! will-quit true))
+
 (defn open-file [filename]
   (reset! buffer (clojure.string/split (slurp filename) #"\n")))
 
@@ -15,7 +24,21 @@
   (let [[w h] (t/get-size term)]
     (t/move-cursor term 0 0)
     (doseq [line (take h @buffer)]
-      (t/put-string term (str line \newline)))))
+      (t/put-string term (str line \newline)))
+    (let [[x y] @cursor]
+      (t/move-cursor term x y))))
+
+(defn bind-key [key action]
+  (swap! key-bindings #(assoc % key action)))
+
+(defn execute-action [key]
+  (let [action (get @key-bindings key)] (if action (action))))
+
+(bind-key \q (fn [] (quit-editor)))
+(bind-key :up (fn [] (move-cursor 0 -1)))
+(bind-key :right (fn [] (move-cursor 1 0)))
+(bind-key :left (fn [] (move-cursor -1 0)))
+(bind-key :down (fn [] (move-cursor 0 1)))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -23,5 +46,6 @@
   ([filename]
    (open-file filename)
    (t/in-terminal term
-                  (render-buffer)
-                  (t/get-key-blocking term))))
+                  (while (not @will-quit)
+                    (render-buffer)
+                    (execute-action (t/get-key-blocking term))))))
