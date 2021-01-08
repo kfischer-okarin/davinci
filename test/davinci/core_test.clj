786124d8d832f2fd70408c9f6d0bc5e0411a6eba
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-01-08 18:07:16 +0900

    Add some mocks

--- test/davinci/core_test.clj
+++ test/davinci/core_test.clj
@@ -1,3 +1,11 @@
 (ns davinci.core-test
   (:require [clojure.test :refer :all]
-            [davinci.core :refer :all]))
+            [davinci.core :refer :all]
+            [davinci.terminal :as t]))
+
+; (deftest test-render-lines-in-rect
+;   (let [operations (atom [])
+;         record-operation (fn [name args] (swap! operations #(conj % (into [name] args))))]
+;     (with-redefs [t/put-string (fn [_ & args] (record-operation :put-string args))]
+;       (render-lines-in-rect :term)
+;       (is (= @operations [[:put-string]])))))
