769efb8fc404dc5818a28729af7481d695633ba7
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-01-09 15:43:33 +0900

    Rewrite tests to use Terminal proxy

--- test/davinci/core_test.clj
+++ test/davinci/core_test.clj
@@ -1,9 +1,15 @@
 (ns davinci.core-test
+  (:import
+   com.googlecode.lanterna.terminal.Terminal)
   (:require [clojure.test :refer :all]
             [davinci.core :refer :all]
             [davinci.editor :as e]
             [davinci.system :as s]
             [davinci.terminal :as t]))
+
+(defmethod print-method
+  java.lang.Object [object writer]
+  (print-simple (str "<#" (.getSimpleName (.getClass object)) ">") writer))
 
 (defn record-function-call [name output-atom]
   (fn [& args] (swap! output-atom #(conj % (into [name] args)))))
@@ -16,11 +22,11 @@
 (def do-nothing (constantly nil))
 
 (deftest test-integration
-  (let [operations (atom nil) keys (atom nil)]
+  (let [operations (atom nil) keys (atom nil) term (proxy [Terminal] [])]
     (with-redefs [t/put-string (record-function-call :put-string operations)
                   s/get-tempfile do-nothing
                   slurp (constantly "My Text\nLine 2\n")
-                  t/get-terminal (constantly :term)
+                  t/get-terminal (constantly term)
                   t/add-resize-listener do-nothing
                   t/get-size (constantly [80 4])
                   t/start do-nothing
@@ -34,26 +40,26 @@
       (main "test.txt")
       (reset! last-key nil)
       (is (= @operations [; Initial render
-                          [:put-string :term "My Text\n"]
-                          [:put-string :term "Line 2\n"]
-                          [:put-string :term "\n"]
-                          [:move-cursor :term 0 3]
-                          [:put-string :term "test.txt:1:1                            " :white :red]
-                          [:put-string :term "                              Last key: " :white :red]
-                          [:move-cursor :term 0 0]
+                          [:put-string term "My Text\n"]
+                          [:put-string term "Line 2\n"]
+                          [:put-string term "\n"]
+                          [:move-cursor term 0 3]
+                          [:put-string term "test.txt:1:1                            " :white :red]
+                          [:put-string term "                              Last key: " :white :red]
+                          [:move-cursor term 0 0]
                           ; After pressing a
-                          [:put-string :term "aMy Text\n"]
-                          [:put-string :term "Line 2\n"]
-                          [:put-string :term "\n"]
-                          [:move-cursor :term 0 3]
-                          [:put-string :term "test.txt:1:2                            " :white :red]
-                          [:put-string :term "     Last key: {:key \\a, :modifiers #{}}" :white :red]
-                          [:move-cursor :term 1 0]
+                          [:put-string term "aMy Text\n"]
+                          [:put-string term "Line 2\n"]
+                          [:put-string term "\n"]
+                          [:move-cursor term 0 3]
+                          [:put-string term "test.txt:1:2                            " :white :red]
+                          [:put-string term "     Last key: {:key \\a, :modifiers #{}}" :white :red]
+                          [:move-cursor term 1 0]
                           ; After pressing F15 (no function)
-                          [:put-string :term "aMy Text\n"]
-                          [:put-string :term "Line 2\n"]
-                          [:put-string :term "\n"]
-                          [:move-cursor :term 0 3]
-                          [:put-string :term "test.txt:1:2                            " :white :red]
-                          [:put-string :term "   Last key: {:key :f15, :modifiers #{}}" :white :red]
-                          [:move-cursor :term 1 0]])))))
+                          [:put-string term "aMy Text\n"]
+                          [:put-string term "Line 2\n"]
+                          [:put-string term "\n"]
+                          [:move-cursor term 0 3]
+                          [:put-string term "test.txt:1:2                            " :white :red]
+                          [:put-string term "   Last key: {:key :f15, :modifiers #{}}" :white :red]
+                          [:move-cursor term 1 0]])))))
